9d4312800792256d2600ffd36a83c1308e3bacb5
Alexander Wiegman <7548448+MadIceTea@users.noreply.github.com>
2018-10-23 11:40:04 +0900

    first

---
+++ Day_FosterCity_1987.js
@@ -0,0 +1,67 @@
+var fostercity_region = /* color: #d63000 */ ee.Geometry.Polygon(
+        [[[-122.984, 38.426],
+          [-120.816, 38.111],
+          [-121.315, 36.516],
+          [-123.439, 36.825],
+          [-122.984, 38.426]]]);
+
+//bands to be considered
+var Landsat_8_BANDS = ['B2', 'B3','B4','B5','B6','B7'];
+var Landsat_7_BANDS = ['B1','B2','B3','B4','B5','B7'];
+var Landsat_5_BANDS = ['B1','B2','B3','B4','B5','B7'];
+var STD_NAMES = ['blue','green','red','nir','swir1','swir2'];
+
+//filtering Foster City Bay-region
+var collection = ee.ImageCollection('LANDSAT/LT5_SR') //load collection 1 - LANDSAT7 raws for a single year
+	.filter(ee.Filter.eq('wrs_path',44))
+	.filter(ee.Filter.eq('wrs_row',34))
+	.filterDate("1986-11-01","1987-10-17")
+	// Filter cloudy scenes.
+  .filter(ee.Filter.lt('CLOUD_COVER', 1))
+	.select(Landsat_5_BANDS, STD_NAMES);
+
+print(collection); //date debug
+
+//Display the Composite
+Map.addLayer(collection, {'bands':['red','blue','green'],min:0,max:2000}, 'baselayer', 0);
+
+var inputimage = collection.median();
+
+function addNDVI(image) {
+  return image
+    .addBands(image.normalizedDifference(['nir','red']).rename('ndvi'))
+  ;
+}
+
+var ndvi = addNDVI(inputimage);
+
+Map.addLayer(ndvi,{bands:['ndvi'],min:0,max:1}, 'ndvilayer', 0);
+
+//predict bands
+var predictionBands = ['blue','green','red','nir','swir1','swir2','ndvi'];
+
+var trainingimage = ndvi.select(predictionBands);
+
+var trainingpolygons = ee.FeatureCollection('ft:16JkY8nPmU8QhcqFIsOD9WWXTJJ5rI4OvUNvtbhZU');
+
+var training = trainingimage.sampleRegions({
+    collection: trainingpolygons,
+    properties: ['class'],
+    scale: 160
+});
+
+//Train the CART classifier (a regular expression, not made up) with default parameters
+var trained = ee.Classifier.cart().train(training,'class', predictionBands);
+
+//Classify image with the same bands used for training.
+var CARTclassified = trainingimage.select(predictionBands).classify(trained);
+
+//Display the result
+Map.addLayer(CARTclassified, {min: 0, max: 3, palette: ['97CAf9','784800', '228B22', 'fff44f']}, 'CARTclassification', 1);
+
+Export.image.toDrive({
+  image: CARTclassified,
+  description: 'classified_image',
+  region:fostercity_region,
+  scale:50.0
+});
---
+++ Night_FosterCity_MODERN.js
@@ -0,0 +1,41 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var fostercity_region = /* color: #d63000 */ee.Geometry.Polygon(
+        [[[-122.984, 38.426],
+          [-120.816, 38.111],
+          [-121.315, 36.516],
+          [-123.439, 36.825],
+          [-122.984, 38.426]]]);
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
+//filtering Foster City Bay-region
+/*var geometry = color: #d63000 ee.Geometry.Polygon(
+        [[[-122.984, 38.426],
+          [-120.816, 38.111],
+          [-121.315, 36.516],
+          [-123.439, 36.825],
+          [-122.984, 38.426]]]);
+// (See import record for the final version of the above.)
+*/
+
+// Filter dates
+    var startyear = 2016,
+        endyear = 2017,
+        month = 11,
+        day = 14,
+        startdate = ee.Date.fromYMD(startyear, month, day),
+        enddate = ee.Date.fromYMD(endyear, month, day);
+
+var viirs = ee.Image('NOAA/VIIRS/DNB/MONTHLY_V1/VCMCFG/20170901'); //load nightime data
+
+Map.addLayer(viirs,{bands:[],min:0,max:1}, 'nightmap', 0);
+var specific = viirs.select('avg_rad');
+Map.addLayer(specific,{bands:['avg_rad'],min:0,max:1},'masked', 1);
+
+print(specific);
+print(viirs); //debug steps
+
+Export.image.toDrive({
+  image: specific,
+  description: 'nightmap_2017',
+  scale:50.0,
+  region:fostercity_region
+});
