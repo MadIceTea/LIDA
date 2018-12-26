4abe845d99d4d482e3ef8f6546136afe9874502c
alex <alex@g.ecc.u-tokyo.ac.jp>
2018-12-26 08:54:52 +0000

    Updated Day_FosterCity_1984.js (added 1 line, deleted 1 line).

--- Day_FosterCity_1984.js
+++ Day_FosterCity_1984.js
@@ -29,7 +29,7 @@ var inputimage = collection.median();
 
 function addNDVI(image) {
   return image
-    .addBands(image.normalizedDifference(['blue','red']).rename('ndvi'))
+    .addBands(image.normalizedDifference(['sw1','red']).rename('ndvi'))
   ;
 }
 
