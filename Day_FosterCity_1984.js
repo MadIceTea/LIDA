440b35020645f2dbe04f39a5e13730c857e3998a
alex <alex@g.ecc.u-tokyo.ac.jp>
2018-12-26 01:53:03 +0000

    Updated Day_FosterCity_1984.js (added 1 line, deleted 1 line).

--- Day_FosterCity_1984.js
+++ Day_FosterCity_1984.js
@@ -12,7 +12,7 @@ var Landsat_5_BANDS = ['B1','B2','B3','B4','B5','B7'];
 var STD_NAMES = ['blue','green','red','nir','swir1','swir2'];
 
 //filtering Foster City Bay-region
-var collection = ee.ImageCollection('LANDSAT/LT5_SR') //load collection 1 - LANDSAT7 raws for a single year
+var collection = ee.ImageCollection('LANDSAT/LT05/C01/T1_SR') //load collection 1 - LANDSAT7 raws for a single year
 	.filter(ee.Filter.eq('wrs_path',44))
 	.filter(ee.Filter.eq('wrs_row',34))
 	.filterDate("1984-01-01","1986-11-01")
