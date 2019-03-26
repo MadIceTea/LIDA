0df75a3b0db8e55dc41c1d854087757bfd56075d
alex <alex@g.ecc.u-tokyo.ac.jp>
2019-03-26 17:20:39 +0000

    Updated Day_FosterCity_1984.js (added 2 lines, deleted 2 lines).

--- Day_FosterCity_1984.js
+++ Day_FosterCity_1984.js
@@ -11,8 +11,8 @@ var STD_NAMES = ['blue','green','red','nir','swir1','swir2'];
 
 //filtering Foster City Bay-region
 var collection = ee.ImageCollection("LANDSAT/LT04/C01/T1_SR") //load collection 1 - LANDSAT7 raws for a single year
-	.filter(ee.Filter.eq('wrs_path',44))
-	.filter(ee.Filter.eq('wrs_row',34))
+	//.filter(ee.Filter.eq('wrs_path',44))
+	//.filter(ee.Filter.eq('wrs_row',34))
 	.filterDate("1984-01-01","1986-11-01")
 	// Filter cloudy scenes.
   .filter(ee.Filter.lt('CLOUD_COVER_LAND', 0.5))
