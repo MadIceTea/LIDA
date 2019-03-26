1a6a46cf1f49ac78ee6000aa7e52afe0f5379767
alex <alex@g.ecc.u-tokyo.ac.jp>
2019-03-26 21:19:48 +0000

    Updated Day_FosterCity_1994.js (added 1 line, deleted 1 line).

--- Day_FosterCity_1994.js
+++ Day_FosterCity_1994.js
@@ -10,7 +10,7 @@ var Landsat_5_BANDS = ['B1','B2','B3','B4','B5','B7'];
 var STD_NAMES = ['blue','green','red','nir','swir1','swir2'];
 
 //filtering Foster City Bay-region
-var collection = ee.ImageCollection("LANDSAT/LT05/C01/T1_SR") //load collection 1 - LANDSAT5 raws for a few years
+var collection = ee.ImageCollection("LANDSAT/LT05/C01/T1_SR") //load collection 1 - LANDSAT5 raw
 	.filter(ee.Filter.eq('WRS_PATH',44))
 	.filter(ee.Filter.eq('WRS_ROW',34))
 	.filterDate("1994-01-01","1994-12-31")
